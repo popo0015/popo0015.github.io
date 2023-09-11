a8c1ce3ebbbb7089f530767ef95510bc0dace367
popo0015 <popo0015@hz.nl>
2023-09-11 23:29:58 +0200

    add

--- dashboard-progress.js
+++ dashboard-progress.js
@@ -1,18 +1,16 @@
-function load(){
-    let button = document.getElementById('submit-button');
+function load() {
+    let button = document.getElementsByClassName('submit-button')[0];
 
-    let grades=[];
-    
-    button.addEventListener('click', ()=>{
-        let text_box =  document.querySelector('body > section:nth-child(3) > div > table:nth-child(1) > tbody > tr:nth-child(2) > td:nth-child(5) > input[type=text]');
+    let grades = [];
+
+    button.addEventListener('click', () => {
+        let text_box = document.querySelector('body > section:nth-child(3) > div > table:nth-child(1) > tbody > tr:nth-child(2) > td:nth-child(5) > input[type=text]');
         let td = document.querySelector('body > section:nth-child(3) > div > table:nth-child(1) > tbody > tr:nth-child(2) > td:nth-child(3)');
-        if (text_box.value === '-'){
+        if (text_box.value === '-') {
             td.style.background = 'yellow';
-        }
-        else if(Number(text_box.value)>= 5.5){
+        } else if (Number(text_box.value) >= 5.5) {
             td.style.background = 'green';
-        }
-        else (Number(text_box.value) <= 5.5){
+        } else if (Number(text_box.value) < 5.5) { // Changed this line to 'else if'
             td.style.background = 'red';
         }
 
