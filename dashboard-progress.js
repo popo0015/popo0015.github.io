6448acefc5af48dfd92a2c4dd6809a1460b3e68b
popo0015 <popo0015@hz.nl>
2023-09-12 17:36:05 +0200

    add

--- dashboard-progress.js
+++ dashboard-progress.js
@@ -1,19 +1,72 @@
 function load() {
-    let button = document.getElementsByClassName('submit-button')[0];
-
+    let button = document.getElementById('submit-button');
+    let objects=[];
     let grades = [];
-
-    button.addEventListener('click', () => {
-        let text_box = document.querySelector('body > section:nth-child(3) > div > table:nth-child(1) > tbody > tr:nth-child(2) > td:nth-child(5) > input[type=text]');
-        let td = document.querySelector('body > section:nth-child(3) > div > table:nth-child(1) > tbody > tr:nth-child(2) > td:nth-child(3)');
-        if (text_box.value === '-') {
-            td.style.background = 'yellow';
-        } else if (Number(text_box.value) >= 5.5) {
-            td.style.background = 'green';
-        } else if (Number(text_box.value) < 5.5) { // Changed this line to 'else if'
-            td.style.background = 'red';
+    let assessments = document.querySelectorAll('.assessment');
+    for (const assessment of assessments) {
+        let obj = {};
+        obj.exam = assessment;
+        obj.ec = 0;
+        obj.grade = 0;
+        obj.box = null;
+        obj.btn = null;
+        let leg = assessment.parentElement.children.length;
+        if(leg<=3){
+            obj.ec = assessment.parentElement.children[1];
+            obj.box = assessment.parentElement.children[2].children[0];
+            obj.btn = assessment.parentElement.children[2].children[1].children[0];
+            obj.btn.addEventListener('click', handler);
         }
+        else if(leg === 6){
+            obj.box = assessment.parentElement.children[5].children[0];
+            obj.btn = assessment.parentElement.children[5].children[1].children[0];
+            obj.btn.addEventListener('click', handler);
+            obj.ec = assessment.parentElement.children[4];
+        }
+        else{
+            obj.box = assessment.parentElement.children[4].children[0];
+            obj.btn = assessment.parentElement.children[4].children[1].children[0];
+            obj.btn.addEventListener('click', handler);
+            obj.ec = assessment.parentElement.children[3];
+        }
+        objects.push(obj);
+    }
 
-        grades.push(Number(text_box.value));
-    })
+    async function handler(e) {
+        let currObj = null;
+        for (const obj of objects) {
+            if(obj.btn.isSameNode(e.target)){
+                currObj = obj;
+                break;
+            }
+        }
+        let value = currObj.box.value;
+        currObj.grade =0;
+        if(value === '-') {
+            currObj.exam.style.background = '#DB983B';
+            currObj.grade = '?';
+        } 
+        else if(Number(value) >= 5.5) {
+            currObj.exam.style.background = '#21B6A8';
+            currObj.grade = Number(value);
+        } 
+        else {
+            currObj.grade = Number(value);
+            currObj.exam.style.background = '#DF505C';
+        }
+        currObj.btn.classList.add('disappear');
+        currObj.box.classList.add('disappear');
+        currObj.btn.parentElement.classList.add('disappear');
+        let p = document.createElement('p');
+        if(currObj.grade === '?'){
+            p.textContent = 'Pending...';
+        }
+        else{
+            p.textContent = currObj.grade;
+        }
+        p.classList.add('show-p');
+        await new Promise(r => setTimeout(r, 2000));
+        currObj.box.parentElement.appendChild(p);
+    }
+
 }
