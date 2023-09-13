48c3b63933484a754fe108f37e601848f434a895
popo0015 <popo0015@hz.nl>
2023-09-13 15:39:19 +0200

    add

--- dashboard-progress.js
+++ dashboard-progress.js
@@ -2,11 +2,12 @@ function load() {
     let button = document.getElementById('submit-button');
     let objects=[];
     let grades = [];
+    let barUpdates = 0;
     let assessments = document.querySelectorAll('.assessment');
     for (const assessment of assessments) {
         let obj = {};
         obj.exam = assessment;
-        obj.ec = 0;
+        obj.ec = null;
         obj.grade = 0;
         obj.box = null;
         obj.btn = null;
@@ -21,7 +22,7 @@ function load() {
             obj.box = assessment.parentElement.children[5].children[0];
             obj.btn = assessment.parentElement.children[5].children[1].children[0];
             obj.btn.addEventListener('click', handler);
-            obj.ec = assessment.parentElement.children[4];
+            obj.ec = assessment.parentElement.children[3];
         }
         else{
             obj.box = assessment.parentElement.children[4].children[0];
@@ -62,11 +63,18 @@ function load() {
             p.textContent = 'Pending';
         }
         else{
+            if(currObj.grade>=5.5){
+                let width = Number(currObj.ec.textContent);
+                barUpdates+=width;
+                let bar = document.getElementsByClassName('progressbar')[0];
+                bar.style.width = barUpdates*10/6 + '%';
+            }
             p.textContent = currObj.grade;
         }
         p.classList.add('show-p');
         await new Promise(r => setTimeout(r, 2000));
         currObj.box.parentElement.appendChild(p);
+        
     }
 
 }
