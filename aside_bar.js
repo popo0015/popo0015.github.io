c1155adff0eb5c75608e415f2ff42edb854721b2
popo0015 <popo0015@hz.nl>
2023-09-13 23:34:15 +0200

    add

--- aside_bar.js
+++ aside_bar.js
@@ -1,7 +1,11 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.height = "100vh";
+    document.getElementById("mySidenav").style.display = "block";
+    document.getElementById("mySidenav").style.transition = "2s";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.display = "none";
 }
